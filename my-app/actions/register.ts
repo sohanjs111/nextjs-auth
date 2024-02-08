16f1169acc1b88f1f0b5e384a1aa47b8fa216217
Sohan Saldanha <sohanjs111@gmail.com>
2024-02-08 04:24:55 +0100

    creating users

--- my-app/actions/register.ts
+++ my-app/actions/register.ts
@@ -1,16 +1,39 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
+    // confirm the fields are not invalid
     if (!validatedFields.success) {
         return { error: "Invalid fields!" };
     }
 
+    // extract the validation fields
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    // check for the existing user in the database
+    const existingUser = await getUserByEmail(email);
+    // If there is an existing user with this email, send back an error message
+    if (existingUser) {
+        return { error: "This account already exists!" };
+    }
 
-    return { success: "Email sent!" };
+    await  db.user.create({ 
+        data: { 
+            name, 
+            email, 
+            password: hashedPassword 
+        }, 
+    });
+
+    // TODO: Send verification taken email
+
+    return { success: "User Created!" };
 };
