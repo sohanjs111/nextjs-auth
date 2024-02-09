5b1dfb815ff7eb690ac1f885e84e3ddb63104df7
Sohan Saldanha <sohanjs111@gmail.com>
2024-02-09 16:43:02 +0100

    SignIn for google and github
    Google:
    API and service
    
    Github:
    OAuth apps
    
    Added Events to auto verify the  email address when signs up with Google or Github,

--- my-app/auth.ts
+++ my-app/auth.ts
@@ -17,6 +17,14 @@ export const {
     signIn, 
     signOut,
 } = NextAuth({
+    events: {
+        async linkAccount({ user }){
+            await db.user.update({
+                where: { id: user.id },
+                data: { emailVerified: new Date() }
+            })
+        }
+    },
     callbacks: {
         async session({token, session}) {
             
