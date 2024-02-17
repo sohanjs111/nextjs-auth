4268fe2786be528d5fcd7395d983e103cf4436d0
Sohan Saldanha <sohanjs111@gmail.com>
2024-02-17 05:36:12 +0100

    client page completed

---
+++ my-app/app/(protected)/client/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { UserInfo } from "@/components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+
+const ClientPage = () => {
+    const user = useCurrentUser();
+
+    return ( 
+        <UserInfo 
+            label="🧑‍💻 Client component"
+            user={user}
+        />
+     );
+}
+ 
+export default ClientPage;
