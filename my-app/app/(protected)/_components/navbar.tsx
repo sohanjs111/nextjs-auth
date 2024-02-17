0a1a4fa6975c070c007f9b05088a9f611d935b3d
Sohan Saldanha <sohanjs111@gmail.com>
2024-02-17 03:21:52 +0100

    added User-button component

--- my-app/app/(protected)/_components/navbar.tsx
+++ my-app/app/(protected)/_components/navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/components/auth/user-button";
 
 export const Navbar = () => {
   const pathname = usePathname();
@@ -43,7 +44,7 @@ export const Navbar = () => {
             </Link>
             </Button>
         </div>
-        <p>User Button</p>
+        <UserButton />
     </nav>
   );
 };
