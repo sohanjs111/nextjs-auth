5784d9abe84e17633f4149cbdef0a872352ef2c1
Sohan Saldanha <sohanjs111@gmail.com>
2024-02-18 12:39:27 +0100

    Added Admin page, Toaster display for api and server action

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/sonner";
 import { auth } from "@/auth";
 import "./globals.css";
 
@@ -21,7 +22,10 @@ export default async function RootLayout({
   return (
     <SessionProvider session={session}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </SessionProvider>
   );
